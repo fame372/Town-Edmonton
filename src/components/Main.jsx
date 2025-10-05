89b47ef72849c62cf84eb3fd67943a5377ed17b3
fame372 <70764830+fame372@users.noreply.github.com>
2025-10-05 14:25:15 -0600

    fixed broken link of two images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,20 +12,20 @@ export default function Main(){
                 <p className="card"> 
                     Let us go and tour gallary and the see the old bones of mamonths
                 </p>
-                <a href="https://www.youraga.ca/exhibitions/past-exhibitions">  Check it out</a>
+                <a href="https://www.youraga.ca/exhibitions/past-exhibitions">Check it out</a>
             </div>
             <div>
-                <img src="images/dji-2572410_1280.jpg"/>
+                <img src="./src/images/dji-2572410_1280.jpg"/>
                 <h3>
                     Muttart Conservatory
                 </h3>
                 <p className="card"> 
                    visit and learn about their 700 plants species or check out what's new in the Featured Pyramid.
                 </p>
-                <a href="https://movelearnplay.edmonton.ca/COE/public/category/browse/MUTTART#gsc.tab=0"> check it out yourself</a>
+                <a href="https://movelearnplay.edmonton.ca/COE/public/category/browse/MUTTART#gsc.tab=0">Check it out</a>
             </div>
             <div>
-                <img src="images/edmonton-84360_1280.jpg"/>
+                <img src="./src/images/edmonton-84360_1280.jpg"/>
                 <h3>
                      Edmonton RiverBoat 
                 </h3>
