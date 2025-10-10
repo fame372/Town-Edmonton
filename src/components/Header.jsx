14c9429f9c3c4e1d7c845fcb6d324126aafcc143
Fadumo Ahmed <70764830+fame372@users.noreply.github.com>
2025-10-10 14:09:40 -0600

    Update Header.jsx
    made the code more organized.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 export default function Header() {
      return (
     <>
-    
-    <section className="section-one">
-        <h1> 
+        <section className="section-one">
+            <h1> 
             Come see Edmonton
-        </h1>
-        <p className="header-paragraph">
-            We have the world's 23rd largest mall in the world. There is something for everybody
-        </p>
-    </section>
+            </h1>
+            <p className="header-paragraph">
+            We have the world's 23rd largest mall in the world. There is something for everybody.
+            </p>
+        </section>
        
     </>
      )
